8841e8d673c97765c425220724374c69e86d6b8f
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-16 13:24:04 +0200

    refactor: rename namespace to ExceptionNotification::Once::Campfire

--- lib/exception_notification/once/campfire/version.rb
+++ lib/exception_notification/once/campfire/version.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module ExceptionNotification
-  module CampfireOnce
-    VERSION = "0.1.0"
+  module Once
+    module Campfire
+      VERSION = "0.1.0"
+    end
   end
 end
