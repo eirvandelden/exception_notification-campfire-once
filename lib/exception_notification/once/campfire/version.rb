621a0ecda979cda44757e00b9e287e7a605948b4
Etienne van Delden de la Haije <etienne@vandelden.family>
2026-07-16 16:01:32 +0200

    chore: O'Saasy license, git-SHA versioning, Ruby EOL check action

--- lib/exception_notification/once/campfire/version.rb
+++ lib/exception_notification/once/campfire/version.rb
@@ -3,7 +3,9 @@
 module ExceptionNotification
   module Once
     module Campfire
-      VERSION = "0.1.0"
+      BASE_VERSION = "0.1.0"
+      sha = `git -C #{__dir__} rev-parse --short HEAD 2>/dev/null`.strip
+      VERSION = sha.empty? ? BASE_VERSION : "#{BASE_VERSION}.pre.git.#{sha}"
     end
   end
 end
